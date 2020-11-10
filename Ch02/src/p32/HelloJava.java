b4fcf74eee13312fb95cfe2029fb64e8237196cb
traver9044@gmail.com <traver9044@gmail.com>
2020-11-10 12:18:41 +0900

    2020/11/10 Ch02

---
+++ Ch02/src/p32/HelloJava.java
@@ -0,0 +1,16 @@
+package p32;
+
+/**
+ * 
+ * date : 2020/11/02
+ * name : 박준우
+ * 내용 : 교재 p32 따라하기
+ *
+ */
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		System.out.println("안녕 자바");
+	}
+}
---
+++ Ch02/src/sub1/Hellojava.java
@@ -0,0 +1,15 @@
+package sub1;
+
+/** 
+ * 
+ * date : 2020/11/02
+ * name : 박준우
+ * 내용 : Ch02, 자바 설치 예제 교재 p31
+ *
+ */
+public class Hellojava {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+	}
+}
