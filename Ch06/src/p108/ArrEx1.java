bb54a9a4ac639c2d4ccd92af5822b7f908b76ce7
traver9044@gmail.com <traver9044@gmail.com>
2020-11-10 12:20:46 +0900

    2020/11/10 Ch06

---
+++ Ch06/src/p108/ArrEx1.java
@@ -0,0 +1,14 @@
+package p108;
+
+public class ArrEx1 {
+	
+	public static void main(String[] args) {
+		
+		int[] arrInt = new int[5];
+		int arrInt2[] = null;
+		
+		System.out.println(arrInt[0]);
+		System.out.println(arrInt2[0]);
+	}
+
+}
---
+++ Ch06/src/p113/ArrEx8.java
@@ -0,0 +1,17 @@
+package p113;
+
+public class ArrEx8 {
+	public static void main(String[] args) {
+		
+		int[] number = new int[100];
+		
+		for(int i=0; i<number.length; i++) {
+			number[i] = i+1;
+			
+		}
+		for(int i=0; i<number.length; i++) {
+			System.out.println(number[i]);
+		}
+	}
+
+}
