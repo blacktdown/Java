9484627c25d7a479df12642914bb160e0a96b797
traver9044@gmail.com <traver9044@gmail.com>
2020-11-10 12:19:34 +0900

    2020/11/10 Ch03

---
+++ Ch03/src/p41/FloatDoubleEx.java
@@ -0,0 +1,16 @@
+package p41;
+
+public class FloatDoubleEx {
+
+	public static void main(String[] args) {
+		
+		float f = 3.1415926535897933f;
+		double d = 3.141592653589793d;
+		
+		System.out.println("float : "+f);
+		System.out.println("double : "+d);
+		
+		double d2 = 3.141592653589793;
+		System.out.println("double : "+d2);
+	}
+}
---
+++ Ch03/src/sub1/VariableTest.java
@@ -0,0 +1,18 @@
+package sub1;
+
+/*
+ * date : 2020/11/02
+ * name : 박준우
+ * 내용 : 교재 p36 변수 실습하기
+ */
+public class VariableTest {
+
+	public static void main(String[] args) {
+		
+		int num1 = 1;
+		int num2 = 2;
+		
+		int num3 = num1 + num2;
+		System.out.println("num3 : "+num3);
+	}
+}
