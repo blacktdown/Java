8c08509028108e62098aa169a1ea7173188ecf50
traver9044@gmail.com <traver9044@gmail.com>
2020-11-10 12:17:24 +0900

    2020/11/10 Ch01

---
+++ ch01/src/sub1/Helloworld.java
@@ -0,0 +1,15 @@
+package sub1;
+
+/*
+ *  date : 2020/11/02
+ *  name : 박준우
+ *  내용 : Ch01 자바 Hello World 출력하기
+ */
+public class Helloworld {
+
+		public static void main(String[] args) {
+			
+			System.out.println("Hello world");	//sysout ctrl+space
+			
+		}
+}
